9145b7a67fb3507b93741c0fffa2097d7c89b3bf
Haolin Ma <michaelfilmic@gmail.com>
2021-01-21 16:48:35 -0500

    organize docker compsoe format

--- app/constant.py
+++
@@ -1,6 +0,0 @@
-PERSON_GROUP_ID = 'standalone_backend'
-RECOGNITION_MODEL = 'recognition_03'
-KEEP_CACHE_PHOTO = 0
-REG_PHOTO_FOLDER = "./cache_register_photo/"
-PAY_PHOTO_FOLDER = "./cache_payment_photo/"
-
